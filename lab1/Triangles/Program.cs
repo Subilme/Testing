62c1802a539f863894038f64d0474732f58f032d
Subilme <leukhin95@list.ru>
2022-12-08 08:27:32 +0300

    Add lab1

---
+++ lab1/Triangles/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Triangles
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] s = new string[] { "" };
+            Console.WriteLine(IfTriangle(s));
+        }
+
+        public static string IfTriangle(string[] args)
+        {
+            try
+            {
+                if (args.Length != 3)
+                {
+                    return "ошибка";
+                }
+
+                double a = double.Parse(args[0]);
+                double b = double.Parse(args[1]);
+                double c = double.Parse(args[2]);
+
+                if ((a + b > c) && (b + c > a) && (a + c > b))
+                {
+                    if ((a.Equals(b)) && (b.Equals(c)))
+                    {
+                        return "равносторонний";
+                    }
+                    if ((a.Equals(b)) || (a.Equals(c)) || (b.Equals(c)))
+                    {
+                        return "равнобедренный";
+                    }
+                    return "обычный";
+                }
+                return "не треугольник";
+            }
+            catch
+            {
+                return "ошибка";
+            }
+        }
+    }
+}
---
+++ lab1/TrinaglesTests/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace TrinaglesTests
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = "input.txt";
+            string output = "output.txt";
+
+            using (StreamReader reader = new(input))
+            {
+                string text = reader.ReadToEnd();
+                args = text.Split("\r\n");
+            }
+
+            using (StreamWriter writer = new(output, false))
+            {
+                foreach (string line in args)
+                {
+                    var expected = line.Split("\"")[1];
+                    string[] parts = line.Substring(0, line.IndexOf("\"")).Trim().Split();
+                    string result = Triangles.Program.IfTriangle(parts);
+                    if (result == expected)
+                    {
+                        writer.WriteLine("success");
+                        continue;
+                    }
+                    writer.WriteLine("error");
+                }
+            }
+        }
+    }
+}
